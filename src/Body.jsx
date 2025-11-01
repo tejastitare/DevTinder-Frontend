158ef9c254ab62cf33791b74c8f60f24f9679467
Tejas <tejastitare30@gmail.com>
2025-11-01 15:58:10 +0530

    routing

---
+++ src/Body.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+
+function Body() {
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer/>
+    </div>
+  );
+}
+
+export default Body;
+ 
