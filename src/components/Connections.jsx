b30f8d2b738b993067585318109ffac9cefcd369
Tejas <tejastitare30@gmail.com>
2025-11-03 16:01:33 +0530

    feat: show connection requests

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -24,24 +24,30 @@ function Connections() {
 
   if (!connections) return;
 
-  if (connections.length === 0) return;
+  if (connections.length === 0) return <div>No Connections Found !</div>;
   return (
     <div className="justify-center items-center mt-4 flex flex-col">
       <h1 className="text-3xl font-bold mb-4">Connections</h1>
 
       <div className="card-body text-white text-3xl w-full flex gap-4 items-center">
         {connections.map((connection, index) => (
-          <div className="card shadow-sm items-center w-[30%] bg-base-300">
+          <div key={index} className="card shadow-sm items-center w-[30%] bg-base-300">
             <div className="flex w-full h-full justify-center items-center gap-6">
               <figure className="w-80 p-4">
-                <img className="rounded-full w-full"
+                <img
+                  className="rounded-full w-full h-40"
                   src={connection.photoUrl}
                   alt="Shoes"
                 />
               </figure>
               <div className="w-full space-y-2">
-                <h2 className="card-title">{connection.firstName} {connection.lastName}</h2>
-                <p className="text-xl"> Age - {connection.age} , Gender - {connection.gender}</p>
+                <h2 className="card-title">
+                  {connection.firstName} {connection.lastName}
+                </h2>
+                <p className="text-xl">
+                  {" "}
+                  Age - {connection.age} , Gender - {connection.gender}
+                </p>
                 <p className="text-xl"> About - {connection.about}</p>
               </div>
             </div>
---
+++ src/components/Requests.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect } from "react";
+
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addRequest } from "../utils/requestSlice";
+function Requests() {
+  const requests = useSelector((store) => store.requests);
+  const dispatch = useDispatch();
+
+  const fetchRequests = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/requests/received", {
+        withCredentials: true,
+      });
+
+      dispatch(addRequest(res.data.data));
+    } catch (error) {
+      console.log("Error in fetching requests: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+
+  if (!requests) return;
+
+  if (requests.length === 0) return <div>No Requests Found !</div>;
+  return (
+    <div className="justify-center items-center mt-4 flex flex-col">
+      <h1 className="text-3xl font-bold mb-4">Connections</h1>
+
+      <div className="card-body text-white text-3xl w-full flex gap-4 items-center">
+        {requests.map((request, index) => {
+          const { _id, firstName, lastName, photoUrl, age, gender, about } =
+            request.fromUserId;
+          return (
+            <div
+              key={_id}
+              className="card shadow-sm items-center w-[40%] bg-base-300"
+            >
+              <div className="flex w-full h-full justify-center items-center gap-6 ml-12">
+                <figure className="w-80 p-2">
+                  <img
+                    className="rounded-full w-full h-32"
+                    src={photoUrl}
+                    alt="Image"
+                  />
+                </figure>
+                <div className="w-[80%] space-y-2">
+                  <h2 className="card-title">
+                    {firstName} {lastName}
+                  </h2>
+                  <p className="text-xl">
+                    Age - {age} , Gender - {gender}
+                  </p>
+                  <p className="text-xl"> About - {about}</p>
+                </div>
+                <div className="flex mr-36 gap-6">
+                  <button className="btn btn-primary">Reject</button>
+                  <button className="btn btn-secondary">Accept</button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Requests;
